4a3b7b273ad25867562e2eaca0bc82c60352df22
Koustubha068 <KoustuK@gmail.com>
2024-01-29 18:04:28 -0500

    attempted js

---
+++ script.js
@@ -0,0 +1,9 @@
+const image1 = 'Britney Spears.jpg'
+const title1 = '1823'
+const text1 = 'Her birthday'
+
+function button(image , title,text){
+    document.getElementById('image-re').src = img
+    document.getElementById('title-re').textContext = title
+    document.getElementById('text-re').textContext = text
+}
